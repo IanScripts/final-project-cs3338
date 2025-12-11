a68c5acc4846da3881661ebe95f00814ae1fa73c
EMPgrammer <ianbp2003@gmail.com>
2025-12-11 11:12:54 -0800

    Added Docker-Compose

---
+++ Docker-compose/amber-md-web/amber-runner/runner.py
@@ -0,0 +1,7 @@
+import time
+
+print("Starting Amber Runner Service...")
+time.sleep(3)
+print("Running Amber molecular simulation...")
+time.sleep(3)
+print("Simulation finished. Output stored in /sim_data")
